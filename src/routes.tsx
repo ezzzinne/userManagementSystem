80a2bbcde5e496f54ed7a074281d068c163cdc8f
ezzzinne <cynthia.nwani017@gmail.com>
2025-03-23 20:35:54 +0100

    added changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UserList from "./components/UserList";
 import UserForm from "./components/UserForm";
 
 const AppRoutes: React.FC = () => {
     return (
-        <Router basename="/userManagementSystem">
-            <Routes>
-                <Route path="/" element={<Navigate to="/users" />} />
-                <Route path="/users" element={<UserList />} />
-                <Route path="/add-user" element={<UserForm />} />
-                <Route path="/edit-user/:id" element={<UserForm />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Navigate to="/users" />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/add-user" element={<UserForm />} />
+            <Route path="/edit-user/:id" element={<UserForm />} />
+        </Routes>
     );
 };
 
